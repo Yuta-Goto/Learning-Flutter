33a2d77231d7326914b960a531068dba512993b8
Yuta Goto <network.story.earth@gmail.com>
2022-05-09 21:39:31 +0900

    Big Change!!
    I finished creating TodoList!!
    Adding to tutorials, I also made deleting function!

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,4 +1,243 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  // 最初に表示するWidget
+  runApp(MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // アプリ名
+      title: 'My Todo App',
+      theme: ThemeData(
+        // テーマカラー
+        primarySwatch: Colors.blue,
+      ),
+      // リスト一覧画面を表示
+      home: TodoListPage(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+class TodoListPage extends StatefulWidget{
+
+  //TodoListPage({Key key, this.title}) : super(key: key);
+
+  //final String title;
+
+  @override 
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+// リスト一覧画面用Widget
+class _TodoListPageState extends State<TodoListPage> {
+
+  List<String> todoList =[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Dismissible(
+            direction: DismissDirection.endToStart,
+            background: Container(
+              alignment: AlignmentDirectional.centerEnd,
+              color: Colors.red,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0.0,0.0,10.0,0.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            onDismissed: (direction){
+              setState(() {
+                todoList.removeAt(index);
+              });
+            },
+            key: UniqueKey(),
+            child: Card(
+              child: ListTile(
+                title: Text(todoList[index]),
+              ),
+            ),
+          );
+        },
+          
+          
+        
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context){
+              return TodoAddPage();
+            }),
+          );
+          if(newListText != null){
+
+            setState((){
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+class TodoAddPage extends StatefulWidget{
+  @override 
+  _TodoAddPageState createState() => _TodoAddPageState();
+}
+
+// リスト追加画面用Widget
+class _TodoAddPageState extends State<TodoAddPage> {
+
+  String _text ='';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text(_text,style: TextStyle(color: Colors.blue),),
+            const SizedBox(height: 8),
+            // テキスト入力
+            TextField(
+              onChanged: (String value){
+                setState(() {
+                  _text = value;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                //color: Colors.blue,
+                onPressed: () {
+                  Navigator.of(context).pop(_text);
+                },
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+/*
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'flutter demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.lightBlue,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('***'),
+                  Text('special', style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic)),
+                ],
+              ),
+            ),
+            Text('Bold',style: TextStyle(fontWeight: FontWeight.bold)),
+            Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+            Text('Size',style: TextStyle(fontSize: 36)),
+            Text('Green',style: TextStyle(color: Colors.green)),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(8),
+              margin: EdgeInsets.all(4),
+              
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.red, width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text('TextAlign.right',textAlign: TextAlign.right),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
@@ -8,7 +247,7 @@ void main() => runApp(MaterialApp(
       centerTitle: true,
     ),
     body: Center(
-      child: Text('Hello Ayutaya!'),
+      child: Text('Hello Ayutaya'),
     ),
     floatingActionButton: FloatingActionButton(
       child: Icon(Icons.close),
@@ -19,4 +258,4 @@ void main() => runApp(MaterialApp(
   ),
 ));
 
-//test aaa
+*/
